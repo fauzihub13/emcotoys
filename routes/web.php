72ee288f8772cc9bc3737c01111573b417cd5db7
fauzihub13 <fauziadisaputra3@gmail.com>
2025-01-28 19:39:50 +0700

    feat: add subdomain routing for admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::domain(env('APP_DOMAIN', 'emcotoys.test'))->group(function (){
+    Route::get('/', function () {
+        return 'USER';
+    });
 });
+
+
+Route::domain('admin.'. env('APP_DOMAIN', 'emcotoys.test'))->group(function (){
+    Route::get('/', function () {
+        return 'ADMIN';
+    });
+});
+
+
+
