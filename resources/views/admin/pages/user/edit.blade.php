e5b89b242e60300f9764fc2f80fc172dd98a6dbb
fauzihub13 <fauziadisaputra3@gmail.com>
2025-01-31 10:48:34 +0700

    fix: remove avatar from edit profile page

--- resources/views/admin/pages/user/edit.blade.php
+++ resources/views/admin/pages/user/edit.blade.php
@@ -27,21 +27,7 @@
         </div>
         <section class="section">
             <div class="row">
-                <div class="col-12 col-lg-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-center align-items-center flex-column">
-                                <div class="avatar avatar-2xl">
-                                    <img src="{{ asset('assets/admin/compiled/jpg/2.jpg') }}" alt="Avatar">
-                                </div>
-
-                                <h3 class="mt-3">{{ auth()->user()->name }}</h3>
-                                <p class="text-small">{{ auth()->user()->email  }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-8">
+                <div class="col-12">
                     @if (session('status') == 'profile-information-updated')
                         <div class="alert alert-success alert-dismissible show fade">
                             Profile updated successfully.
