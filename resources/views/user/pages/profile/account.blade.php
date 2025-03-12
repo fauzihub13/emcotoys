d66791236dbe1a5cfd1dd0cc11405f600f995098
fauzihub13 <fauziadisaputra3@gmail.com>
2025-03-12 09:45:56 +0700

    fix: responsive on user profile page

--- resources/views/user/pages/profile/account.blade.php
+++ resources/views/user/pages/profile/account.blade.php
@@ -1,6 +1,6 @@
 @extends('user.layouts.app')
 
-@section('title', 'cart')
+@section('title', 'Cart')
 
 @push('style')
     <link rel="stylesheet" href="{{ asset ('template/assets/css/profile.css')}}">
@@ -15,31 +15,31 @@
             </div>
             <div class="col-lg-9">
                 <h2>Profile</h2>
-                <div class="container form-text shadow-lg p-5 pb-4 rounded">
-                    <p class="title text-danger fw-normal">Edit Your Profile</p>
+                <div class="container right-side form-text ">
+                    <p class="title text-danger fs-5 fw-normal">Edit Your Profile</p>
                     <form class=" fs-1">
                         <div class="row">
-                            <div class="col">
-                                <label for="">Name</label>
-                                <input type="text" class="form-control rounded-0" placeholder="Name">
+                            <div class="col-lg-6 col-sm-12">
+                                <label for="name">Name</label>
+                                <input id="name" name="name" type="text" class="form-control rounded-0" placeholder="Name">
                             </div>
-                            <div class="col">
-                                <label for="">Phone Number</label>
-                                <input type="text" class="form-control rounded-0" placeholder="123-456-876">
+                            <div class="col-lg-6 col-sm-12 mt-3 mt-lg-0 mt-md-0">
+                                <label for="phone_number">Phone Number</label>
+                                <input id="phone_number" name="phone_number" type="text" class="form-control rounded-0" placeholder="6289654126325">
                             </div>
                         </div>
                         <div class="row mt-3">
-                            <div class="col">
-                                <label for="">Email</label>
-                                <input type="text" class="form-control rounded-0" placeholder="jone@email.com">
+                            <div class="col-lg-6 col-sm-12">
+                                <label for="email">Email</label>
+                                <input id="email" name="email" type="text" class="form-control rounded-0" placeholder="jone@email.com">
                             </div>
-                            <div class="col">
-                                <label for="">Address</label>
-                                <input type="text" class="form-control rounded-0" placeholder="City, Country">
+                            <div class="col-lg-6 col-sm-12 mt-3 mt-lg-0 mt-md-0">
+                                <label for="address">Address</label>
+                                <input id="address" name="address" type="text" class="form-control rounded-0" placeholder="City, Country">
                             </div>
                         </div>
                         <div class="col mt-3">
-                            <label for="">Change Password</label>
+                            <label>Change Password</label>
                             <input type="text" class="form-control rounded-0" placeholder="Current Password">
                             <input type="text" class="form-control rounded-0 mt-3" placeholder="New Password">
                             <input type="text" class="form-control rounded-0 mt-3" placeholder="Confirm New Password">
